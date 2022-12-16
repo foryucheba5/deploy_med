681128586c28393a331cd112f8f0d4909571447c
foryucheba5 <96658608+foryucheba5@users.noreply.github.com>
2022-12-16 22:19:48 +0400

    Delete client directory

--- client/src/http/userAPI.js
+++
@@ -1,8 +0,0 @@
-import {$authHost, $host} from "./index";
-
-
-export const check = async (password) => {  
-    const {data} = await $host.post(`auth/check?pass=${password}`)
-    localStorage.setItem('data', data)
-    return data
-}
